16b805d40c8c2cc4b5e27c01dc75a8f4ba80fc59
Wan chunjiang <1694978998@qq.com>
2022-11-26 11:41:05 +0800

    second commit

---
+++ Hello.java
@@ -0,0 +1,9 @@
+public class helloworld { 
+   
+    public static void main(String[] args){ 
+   
+        System.out.println("hello world!");
+        System.out.printf("hello world!!\n");
+        System.out.print("hello world!!!");
+    }
+}
